b0b9d80645278259caf0944df421887ebc85025c
Timka-cloud1 <amanzholtimka@gmail.com>
2024-09-10 14:34:41 +0500

    asd

---
+++ src/main/java/kz/timka/App.java
@@ -0,0 +1,13 @@
+package kz.timka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
